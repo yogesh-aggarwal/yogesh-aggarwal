1e7a0195bc31a10e164127da79941c7d035901c7
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-11-27 23:46:57 +0530

    Added navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { HeroComponent } from "./home/hero/hero.component";
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroComponent, HomeComponent],
+  declarations: [AppComponent, HeroComponent, HomeComponent, NavbarComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
