fe88c8f2f2fe6d546b8b2bfda7e86a8cb3717f14
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-11-28 00:23:12 +0530

    Fixed import errors in styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { HomeComponent } from "./home/home.component";
-import { HeroComponent } from "./home/hero/hero.component";
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SidebarsComponent } from './components/sidebars/sidebars.component';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { SidebarsComponent } from "./components/sidebars/sidebars.component";
+import { HeroComponent } from "./routes/home/hero/hero.component";
+import { HomeComponent } from "./routes/home/home.component";
 
 @NgModule({
-  declarations: [AppComponent, HeroComponent, HomeComponent, NavbarComponent, SidebarsComponent],
+  declarations: [
+    AppComponent,
+    HeroComponent,
+    HomeComponent,
+    NavbarComponent,
+    SidebarsComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
