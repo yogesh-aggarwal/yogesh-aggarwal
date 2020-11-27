7cc9098bd25b4527bf090eaa44b5ce47b654c0e9
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-11-27 22:38:28 +0530

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./routes/home/home.component";
+import { HeroComponent } from "./routes/home/hero/hero.component";
+import { ContactComponent } from './routes/contact/contact.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeroComponent, HomeComponent, ContactComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
