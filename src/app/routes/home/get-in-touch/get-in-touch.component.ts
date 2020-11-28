ea5c69c8c486dd362222212d271a7e3c7efa6f2f
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-11-28 18:23:40 +0530

    Added media_query for get-in-touch component

--- src/app/routes/home/get-in-touch/get-in-touch.component.ts
+++ src/app/routes/home/get-in-touch/get-in-touch.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "get-in-touch",
   templateUrl: "./get-in-touch.component.html",
-  styleUrls: ["./get-in-touch.component.scss"],
+  styleUrls: [
+    "./get-in-touch.component.scss",
+    "./get-in-touch.component_media.scss",
+  ],
 })
 export class GetInTouchComponent implements OnInit {
   constructor() {}
