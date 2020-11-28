e4616cf886b7b488d8ea7b5966112786f9599aec
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-11-29 00:26:17 +0530

    Added animations to hero

--- src/app/routes/home/hero/hero.component.ts
+++ src/app/routes/home/hero/hero.component.ts
@@ -8,6 +8,8 @@ import { TweenLite } from "gsap";
   styleUrls: ["./hero.component.scss", "./hero.component_media.scss"],
 })
 export class HeroComponent implements OnInit {
+  @ViewChild("nameHead")
+  nameHead: ElementRef;
   @ViewChild("name")
   name: ElementRef;
   @ViewChild("teaser")
@@ -16,10 +18,43 @@ export class HeroComponent implements OnInit {
   description: ElementRef;
   @ViewChild("socials")
   socials: ElementRef;
+  @ViewChild("cta")
+  cta: ElementRef;
 
   constructor(public dataService: DataService) {}
 
+  animate() {
+    const transitions = {
+      opacity: 0,
+      y: 30,
+      transform: "rotateX(30deg)",
+    };
+    TweenLite.from(this.nameHead.nativeElement, 1, transitions);
+    TweenLite.from(this.name.nativeElement, 1, {
+      ...transitions,
+      delay: 0.1,
+    });
+    TweenLite.from(this.teaser.nativeElement, 1, {
+      ...transitions,
+      delay: 0.2,
+    });
+    TweenLite.from(this.description.nativeElement, 1, {
+      ...transitions,
+      delay: 0.3,
+    });
+    TweenLite.from(this.socials.nativeElement, 1, {
+      ...transitions,
+      delay: 0.4,
+    });
+    TweenLite.from(this.cta.nativeElement, 1, {
+      ...transitions,
+      delay: 0.6,
+    });
+  }
+
   ngOnInit(): void {
-    
+    setTimeout(() => {
+      this.animate();
+    });
   }
 }
