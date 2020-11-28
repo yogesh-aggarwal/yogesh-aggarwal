88b4b98d64f02f462a9a0fb4df19419d1a7675f4
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-11-28 18:15:50 +0530

    Added media query for hero section

--- src/app/routes/home/hero/hero.component.ts
+++ src/app/routes/home/hero/hero.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "hero",
   templateUrl: "./hero.component.html",
-  styleUrls: ["./hero.component.scss"],
+  styleUrls: ["./hero.component.scss", "./hero.component_media.scss"],
 })
 export class HeroComponent implements OnInit {
   constructor() {}
