11e5632eafd9ad7ed8c8bb4bddbbc86479896fc9
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-11-28 17:47:23 +0530

    Made about section responsive

--- src/app/routes/home/about-me/about-me.component.ts
+++ src/app/routes/home/about-me/about-me.component.ts
@@ -4,7 +4,7 @@ import { DataService } from "src/app/services/data.service";
 @Component({
   selector: "about-me",
   templateUrl: "./about-me.component.html",
-  styleUrls: ["./about-me.component.scss"],
+  styleUrls: ["./about-me.component.scss", "./about-me.component_media.scss"],
 })
 export class AboutMeComponent implements OnInit {
   constructor(public dataService: DataService) {}
