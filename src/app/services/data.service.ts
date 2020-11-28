2421cb50ae15f6355d338c33f401709081c2589c
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-11-28 12:31:44 +0530

    Added project templarte

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from "@angular/core";
 
+interface ProjectInterface {
+  name: string;
+  description: string;
+  technologies: string[];
+  githubUrl: string;
+  projectUrl: string;
+  imageUrl: string;
+}
+
 @Injectable({
   providedIn: "root",
 })
 export class DataService {
   email: string = "yogeshdevaggarwal@gmail.com";
-  projects = [];
+  featuredProjects: ProjectInterface[] = [];
+  otherProjects: ProjectInterface[] = [];
   technologies: string[] = [
     "Angular",
     "ReactJS",
