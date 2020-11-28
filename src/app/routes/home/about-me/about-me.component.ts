e6cf824ec4b82fff5a75c44c2285c32f9ef313e1
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-11-29 00:38:45 +0530

    Minor animation changes

--- src/app/routes/home/about-me/about-me.component.ts
+++ src/app/routes/home/about-me/about-me.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { DataService } from "src/app/services/data.service";
+import { TweenLite } from "gsap";
 
 @Component({
   selector: "about-me",
@@ -7,7 +8,31 @@ import { DataService } from "src/app/services/data.service";
   styleUrls: ["./about-me.component.scss", "./about-me.component_media.scss"],
 })
 export class AboutMeComponent implements OnInit {
+  @ViewChild("section")
+  section: ElementRef;
+  @ViewChild("heading")
+  heading: ElementRef;
+
   constructor(public dataService: DataService) {}
 
-  ngOnInit(): void {}
+  animate() {
+    const transitions = {
+      opacity: 0,
+      y: 40,
+      transform: "rotateX(30deg)",
+      duration: 0.1,
+      scrollTrigger: this.section.nativeElement,
+    };
+    TweenLite.from(this.heading.nativeElement, 1, transitions);
+    // TweenLite.from(this.name.nativeElement, 1, {
+    //   ...transitions,
+    //   delay: 0.1,
+    // });
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.animate();
+    });
+  }
 }
