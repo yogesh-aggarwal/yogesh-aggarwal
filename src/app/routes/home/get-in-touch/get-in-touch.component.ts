b684a6859b58873890c3c137068345465943b8bb
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-11-28 18:38:15 +0530

    Added mailto in get-in-touch & hero component

--- src/app/routes/home/get-in-touch/get-in-touch.component.ts
+++ src/app/routes/home/get-in-touch/get-in-touch.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { DataService } from "src/app/services/data.service";
 
 @Component({
   selector: "get-in-touch",
@@ -9,7 +10,7 @@ import { Component, OnInit } from "@angular/core";
   ],
 })
 export class GetInTouchComponent implements OnInit {
-  constructor() {}
+  constructor(public dataService: DataService) {}
 
   ngOnInit(): void {}
 }
