07862612d1112bfebbf4b121883cd0ccc3b32732
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-11-28 17:04:49 +0530

    Added get-in-touch component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeroComponent } from "./routes/home/hero/hero.component";
 import { HomeComponent } from "./routes/home/home.component";
 import { AboutMeComponent } from "./routes/home/about-me/about-me.component";
 import { WorkComponent } from "./routes/home/work/work.component";
+import { GetInTouchComponent } from './routes/home/get-in-touch/get-in-touch.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { WorkComponent } from "./routes/home/work/work.component";
     SidebarsComponent,
     AboutMeComponent,
     WorkComponent,
+    GetInTouchComponent,
   ],
   imports: [BrowserModule],
   providers: [],
---
+++ src/app/routes/home/get-in-touch/get-in-touch.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GetInTouchComponent } from './get-in-touch.component';
+
+describe('GetInTouchComponent', () => {
+  let component: GetInTouchComponent;
+  let fixture: ComponentFixture<GetInTouchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GetInTouchComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GetInTouchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
