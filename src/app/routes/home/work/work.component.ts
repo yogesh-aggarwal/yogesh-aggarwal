3742bdeaeb178af7b017abf65a0d09c092b58aa5
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-11-29 10:55:23 +0530

    Added animations in work section

--- src/app/routes/home/work/work.component.ts
+++ src/app/routes/home/work/work.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from "@angular/core";
-import { DataService } from "src/app/services/data.service";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { DataService } from "../../../services/data.service";
+import { gsap, TweenLite } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 @Component({
   selector: "work",
@@ -7,7 +9,49 @@ import { DataService } from "src/app/services/data.service";
   styleUrls: ["./work.component.scss", "./work.component_media.scss"],
 })
 export class WorkComponent implements OnInit {
+  @ViewChild("heading")
+  heading: ElementRef;
+  @ViewChild("archive")
+  archive: ElementRef;
+  @ViewChild("otherProjects")
+  otherProjects: ElementRef;
+
   constructor(public dataService: DataService) {}
 
-  ngOnInit(): void {}
+  animate() {
+    let transitions: gsap.TweenVars = {
+      opacity: 0,
+      y: 60,
+    };
+    // Featured Projects
+    this.dataService.featuredProjects.forEach((project) => {
+      let projectElement = document.getElementById(
+        project.name.replace(" ", "-")
+      );
+      if (!projectElement) return;
+      TweenLite.from(projectElement, 1, {
+        ...transitions,
+        delay: 0.2,
+        scrollTrigger: projectElement,
+      });
+    });
+    // Other Projects
+    transitions = { ...transitions, scrollTrigger: this.heading.nativeElement };
+    TweenLite.from(this.heading.nativeElement, 1, transitions);
+    TweenLite.from(this.archive.nativeElement, 1, {
+      ...transitions,
+      delay: 0.2,
+    });
+    TweenLite.from(this.otherProjects.nativeElement, 1, {
+      ...transitions,
+      delay: 0.6,
+    });
+  }
+
+  ngOnInit(): void {
+    gsap.registerPlugin(ScrollTrigger);
+    setTimeout(() => {
+      this.animate();
+    });
+  }
 }
