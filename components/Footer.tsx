546cc9160e57ec6e697ce1398f1b251eb415784f
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-04-17 21:41:21 +0530

    Added fiiter

---
+++ components/Footer.tsx
@@ -0,0 +1,69 @@
+export default function Footer() {
+	return (
+		<footer className="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
+			<p>
+				Loosely designed in{" "}
+				<a
+					href="https://www.figma.com/"
+					className="font-medium text-slate-400 hover:text-blue-300 focus-visible:text-blue-300"
+					target="_blank"
+					rel="noreferrer noopener"
+					aria-label="Figma (opens in a new tab)"
+				>
+					Figma
+				</a>{" "}
+				and coded in{" "}
+				<a
+					href="https://code.visualstudio.com/"
+					className="font-medium text-slate-400 hover:text-blue-300 focus-visible:text-blue-300"
+					target="_blank"
+					rel="noreferrer noopener"
+					aria-label="Visual Studio Code (opens in a new tab)"
+				>
+					Visual Studio Code
+				</a>{" "}
+				by yours truly. Built with{" "}
+				<a
+					href="https://nextjs.org/"
+					className="font-medium text-slate-400 hover:text-blue-300 focus-visible:text-blue-300"
+					target="_blank"
+					rel="noreferrer noopener"
+					aria-label="Next.js (opens in a new tab)"
+				>
+					Next.js
+				</a>{" "}
+				and{" "}
+				<a
+					href="https://tailwindcss.com/"
+					className="font-medium text-slate-400 hover:text-blue-300 focus-visible:text-blue-300"
+					target="_blank"
+					rel="noreferrer noopener"
+					aria-label="Tailwind CSS (opens in a new tab)"
+				>
+					Tailwind CSS
+				</a>
+				, deployed with{" "}
+				<a
+					href="https://vercel.com/"
+					className="font-medium text-slate-400 hover:text-blue-300 focus-visible:text-blue-300"
+					target="_blank"
+					rel="noreferrer noopener"
+					aria-label="Vercel (opens in a new tab)"
+				>
+					Vercel
+				</a>
+				. All text is set in the{" "}
+				<a
+					href="https://rsms.me/inter/"
+					className="font-medium text-slate-400 hover:text-blue-300 focus-visible:text-blue-300"
+					target="_blank"
+					rel="noreferrer noopener"
+					aria-label="Inter (opens in a new tab)"
+				>
+					Inter
+				</a>{" "}
+				typeface.
+			</p>
+		</footer>
+	)
+}
