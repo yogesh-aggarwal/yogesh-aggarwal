fd069bce21a56aae660f407809d14b365918098e
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-05-16 12:46:13 +0530

    Added vercel analytics to the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css"
 
+import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from "next"
 
 export const metadata: Metadata = {
@@ -14,6 +15,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<Analytics />
 			<body>{children}</body>
 		</html>
 	)
