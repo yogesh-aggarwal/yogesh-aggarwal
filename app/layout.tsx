a11da92f4662b444c59eabf1ab6e5f19c289197b
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2025-11-17 22:28:00 +0530

    Squashed commit of the following:
    commit d2d63a33804953cb501ac34fed1f06d438412593
    Author: Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
    Date:   Mon Nov 17 22:27:08 2025 +0530
    
        minor change
    
    commit 2904cfc6719714ae8f6c4bbd5a0a686bc1434c29
    Author: Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
    Date:   Mon Nov 17 22:22:39 2025 +0530
    
        fixed apollo settings
    
    commit 51839a060ce505a2e29aead03f7d7be495a5a900
    Author: Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
    Date:   Mon Nov 17 22:19:36 2025 +0530
    
        added vscode settings
    
    commit dca97452503c2c45269798150337a665732fb79a
    Author: Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
    Date:   Mon Nov 17 22:15:06 2025 +0530
    
        added apollo analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,24 @@ export default function RootLayout({
       <html lang="en">
          <Analytics />
          <SpeedInsights />
+         <script
+            dangerouslySetInnerHTML={{
+               __html: `
+                  function initApollo() {
+                     var n = Math.random().toString(36).substring(7);
+                     var o = document.createElement("script");
+                     o.src = "https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache=" + n;
+                     o.async = true;
+                     o.defer = true;
+                     o.onload = function() {
+                        window.trackingFunctions.onLoad({appId: "66b718669fbee9019aab654f"});
+                     };
+                     document.head.appendChild(o);
+                  }
+                  initApollo();
+               `,
+            }}
+         />
          <body>{children}</body>
       </html>
    )
