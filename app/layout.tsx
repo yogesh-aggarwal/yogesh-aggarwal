2904cfc6719714ae8f6c4bbd5a0a686bc1434c29
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2025-11-17 22:22:39 +0530

    fixed apollo settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,12 +43,24 @@ export default function RootLayout({
       <html lang="en">
          <Analytics />
          <SpeedInsights />
-         <script>
-            function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
-            o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
-            o.onload=function(){window.trackingFunctions.onLoad({appId:"66b718669fbee9019aab654f"})},
-            document.head.appendChild(o)}initApollo();
-         </script>
+         <script
+            dangerouslySetInnerHTML={{
+               __html: `
+                  function initApollo() {
+                     var n = Math.random().toString(36).substring(7);
+                     var o = document.createElement("script");
+                     o.src = "https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache=" + n;
+                     o.async = true;
+                     o.defer = true;
+                     o.onload = function() {
+                        window.trackingFunctions.onLoad({appId: "66b718669fbee9019aab654f"});
+                     };
+                     document.head.appendChild(o);
+                  }
+                  initApollo();
+               `,
+            }}
+         />
          <body>{children}</body>
       </html>
    )
